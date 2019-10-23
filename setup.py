acb65002a0ead8904cb01260fe1727782f8434a8
Andrew Moore <andrew.p.moore94@gmail.com>
2019-10-23 16:33:15 +0100

    Python setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(name='pea_classification',
+      version='0.0.1',
+      description='Financial NLP methods for performance, tone, and attribution detection',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/apmoore1/pea_classification',
+      author='Andrew Moore',
+      author_email='andrew.p.moore94@gmail.com',
+      license='Apache License 2.0',
+      install_requires=[
+          'pandas==0.25.1',
+          'xlrd==1.2.0',
+          'scikit-learn==0.21.3',
+          'imbalanced-learn==0.5.0'
+      ],
+      python_requires='>=3.6.1',
+      packages=find_packages(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 3.6'
+      ])
